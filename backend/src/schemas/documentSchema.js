e535f5acfbb543e75cbbf95be798783ebd8d9e09
Nguyễn Minh Nhựt <83703387+talachanvuong@users.noreply.github.com>
2025-02-07 08:27:05 +0700

    Add editDocumentSchema

--- backend/src/schemas/documentSchema.js
+++ backend/src/schemas/documentSchema.js
@@ -30,7 +30,7 @@ export const createDocumentSchema = Joi.object({
                 .required(),
             }).or('text', 'attachment')
           )
-          .unique((a, b) => a.type === b.type)
+          .unique('type')
           .required(),
         correct: Joi.string().trim().valid('A', 'B', 'C', 'D').required(),
       })
@@ -46,3 +46,75 @@ export const getDocumentDetailSchema = Joi.object({
 export const removeDocumentSchema = Joi.object({
   document: Joi.number().integer().strict().min(1).required(),
 })
+
+export const editDocumentSchema = Joi.object({
+  document: Joi.number().integer().strict().min(1).required(),
+  title: Joi.string().trim().min(8).max(256),
+  description: Joi.string().trim().max(2048).allow(null),
+  course: Joi.number().integer().strict().min(1),
+  questions: Joi.array()
+    .items(
+      Joi.object({
+        action: Joi.string().trim().valid('add', 'remove', 'edit').required(),
+      })
+        .when(Joi.object({ action: 'add' }).unknown(), {
+          then: Joi.object({
+            content: Joi.array()
+              .length(5)
+              .items(
+                Joi.object({
+                  text: Joi.string().trim().max(4096),
+                  attachment: Joi.string()
+                    .trim()
+                    .dataUri()
+                    .regex(mimeTypePattern)
+                    .max(maxBase64Length),
+                  type: Joi.string()
+                    .trim()
+                    .valid('A', 'B', 'C', 'D', 'Q')
+                    .required(),
+                }).or('text', 'attachment')
+              )
+              .unique('type')
+              .required(),
+            order: Joi.number().integer().strict().min(1).required(),
+            correct: Joi.string().trim().valid('A', 'B', 'C', 'D').required(),
+          }),
+        })
+        .when(Joi.object({ action: 'remove' }).unknown(), {
+          then: Joi.object({
+            id: Joi.number().integer().strict().min(1).required(),
+          }),
+        })
+        .when(Joi.object({ action: 'edit' }).unknown(), {
+          then: Joi.object({
+            id: Joi.number().integer().strict().min(1).required(),
+            content: Joi.array()
+              .items(
+                Joi.object({
+                  id: Joi.number().integer().strict().min(1).required(),
+                  text: Joi.string().trim().max(4096).allow(null),
+                  attachment: Joi.string()
+                    .trim()
+                    .dataUri()
+                    .regex(mimeTypePattern)
+                    .max(maxBase64Length)
+                    .allow(null),
+                })
+                  .or('text', 'attachment')
+                  .custom((value, helpers) => {
+                    if (value.text === null && value.attachment === null) {
+                      return helpers.error('any.invalid')
+                    }
+                    return value
+                  })
+              )
+              .min(1)
+              .max(5),
+            order: Joi.number().integer().strict().min(1),
+            correct: Joi.string().trim().valid('A', 'B', 'C', 'D'),
+          }).or('order', 'correct', 'content'),
+        })
+    )
+    .min(1),
+}).or('title', 'description', 'course', 'questions')
