dce31ab8e06402b1e49b49c43d9c1cbf0e37162b
Nguyễn Minh Nhựt <83703387+talachanvuong@users.noreply.github.com>
2025-02-15 15:35:54 +0700

    Fix log full error details in errorHandler

--- backend/src/middleware/errorMiddleware.js
+++ backend/src/middleware/errorMiddleware.js
@@ -1,6 +1,6 @@
 import { MESSAGE, sendResponse, STATUS_CODE } from '../utils/constant.js'
 
 export const errorHandler = (err, req, res, next) => {
-  console.error(err.message)
+  console.error(err)
   return sendResponse(res, STATUS_CODE.INTERNAL_SERVER_ERROR, MESSAGE.SERVER.ERROR)
 }
