e87726e919bdf346e45192e028b02bf8cbab6450
NguyenDucTam <tam9898980@gmail.com>
2025-02-18 07:38:52 +0700

    add practice routes

--- backend/src/config/routesConfig.js
+++ backend/src/config/routesConfig.js
@@ -2,10 +2,12 @@ import { Router } from 'express'
 import courseRoutes from '../routes/courseRoutes.js'
 import documentRoutes from '../routes/documentRoutes.js'
 import userRoutes from '../routes/userRoutes.js'
+import practiceRoutes from '../routes/practiceRoutes.js'
 
 const router = Router()
 router.use('/user', userRoutes)
 router.use('/course', courseRoutes)
 router.use('/document', documentRoutes)
+router.use('/practice', practiceRoutes)
 
 export default router
