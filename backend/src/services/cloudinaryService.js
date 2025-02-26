feafd439f03e22d80bed14b12d90ca13fa8af987
Nguyễn Minh Nhựt <83703387+talachanvuong@users.noreply.github.com>
2025-02-26 16:51:39 +0700

    Change param name in cloudinaryService

--- backend/src/services/cloudinaryService.js
+++ backend/src/services/cloudinaryService.js
@@ -1,11 +1,11 @@
 import { v2 as cloudinary } from 'cloudinary'
 
-const upload = async (link) => {
-  if (!link) {
+const upload = async (source) => {
+  if (!source) {
     return
   }
 
-  return await cloudinary.uploader.upload(link)
+  return await cloudinary.uploader.upload(source)
 }
 
 const destroy = async (public_id) => {
