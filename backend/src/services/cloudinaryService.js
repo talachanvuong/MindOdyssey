e6cec69902d9599c2a3dd36c38724c3a663af5aa
NguyenDucTam <tam9898980@gmail.com>
2025-03-01 22:14:23 +0700

    add funcs to reupload

--- backend/src/services/cloudinaryService.js
+++ backend/src/services/cloudinaryService.js
@@ -11,3 +11,34 @@ export const uploadCloudinary = async (link) => {
 export const destroyCloudinary = async (public_id) => {
   await cloudinary.uploader.destroy(public_id)
 }
+
+export const reUploadCloudinary = async (url) => {
+  if (!url) {
+    return
+  }
+  try {
+    const result = await cloudinary.uploader.upload(url)
+    return { url: result.secure_url, public_id: result.public_id }
+  } catch (error) {
+    console.error('Error during upload cloundinary:', error)
+    throw error
+  }
+}
+export const reuploadAttachments = async (data) => {
+  for (const item of data) {
+    const uploadPromises = item.contents.map(async (content) => {
+      if (content.attachment && content.attachment_id) {
+        try {
+          const uploadResult = await reUploadCloudinary(content.attachment)
+          content.attachment = uploadResult.url
+          content.attachment_id = uploadResult.public_id
+        } catch (error) {
+          console.error(`Failed to upload ${content.attachment_id}`, error)
+        }
+      }
+    })
+    await Promise.all(uploadPromises)
+  }
+
+  return data
+}
