6abb0c9146f6473ab82ac6455442c235a6b96865
NguyenDucTam <tam9898980@gmail.com>
2025-03-10 15:21:59 +0700

    refactor

--- backend/src/sockets/practiceSocket.js
+++ backend/src/sockets/practiceSocket.js
@@ -1,87 +1,86 @@
 import practiceService from '../services/practiceService.js'
-import {isDocumentExist,isQuestionExist} from '../services/documentService.js'
+import { isDocumentExist } from '../services/documentService.js'
 import practiceSchema from '../schemas/practiceSchema.js'
 import { MESSAGE } from '../utils/constant.js'
-import {reuploadAttachments} from '../services/cloudinaryService.js'
+import { reuploadAttachments } from '../services/cloudinaryService.js'
 
-const getStarted = async (socket,data,callback)=>{
-  const {user_id} = socket.user
-  const {error,value}=practiceSchema.doc_idValidate.validate(data)
+const getStarted = async (socket, data, callback) => {
+  const { error, value } = practiceSchema.doc_idValidate.validate(data)
   if (error) {
     return socket.emit('error', error.details[0].message)
   }
-  const {doc_id}=value
-  //check doc_id exist
+
+  const { doc_id } = value
   const doc_idExist = await isDocumentExist(doc_id)
-  if(!doc_idExist){
+  if (!doc_idExist) {
     return socket.emit('error', MESSAGE.DOCUMENT.NOT_FOUND)
   }
   const allQuestion = await practiceService.selectAllQuestions(doc_id)
-  //reupload
-  const allQuestionreuploaded = await reuploadAttachments(allQuestion)
- 
-  const practice_history_id = await practiceService.insertPracticeHistory(user_id,0,allQuestionreuploaded)
-  callback(practice_history_id)
+  socket.allQuestionreuploaded = await reuploadAttachments(allQuestion)
+  socket.start_time = new Date().toISOString()
+  socket.questionOrder = []
+  callback('ready')
 }
 
-const getNewQuestion = async (socket,data, callback) => {
-  
-  const {error,value}=practiceSchema.getNewQuestionValidate.validate(data)
-  if (error) {
-    return socket.emit('error', error.details[0].message)
-  }
-  const {doc_id,questionorder}=value
-  const doc_idExist = await isDocumentExist(doc_id)
-  if(!doc_idExist){
-    return socket.emit('error', MESSAGE.DOCUMENT.NOT_FOUND)
+const getNewQuestion = async (socket, callback) => {
+
+  const filteredQuestions = socket.allQuestionreuploaded
+    .filter((q) => !socket.questionOrder.includes(q.order))
+    .map(({ correct_answer, ...rest }) => rest)
+
+  if (filteredQuestions.length === 0) {
+    return socket.emit('error', MESSAGE.PRACTICE.NO_QUESTION)
   }
 
-  const question = await practiceService.sendQuestion(doc_id, questionorder)
-  if (!question) return socket.emit('error', MESSAGE.PRACTICE.NO_QUESTION)
+  const randomIndex = Math.floor(Math.random() * filteredQuestions.length)
+  const question = filteredQuestions[randomIndex]
+
+  socket.questionOrder.push(question.order)
   callback(question)
 }
 
-const postUserAnswer = async (socket,data,callback) => {
-  const { error,value } = practiceSchema.answerValidate.validate(
-    data
-  )
+const postUserAnswer = async (socket, data, callback) => {
+  const { error, value } = practiceSchema.answerValidate.validate(data)
   if (error) {
     return socket.emit('error', error.details[0].message)
   }
-  const {userAnswer,question_id,practice_history_id}=value
-  const practice_history_idExist = await practiceService.ispractice_history_idExist(practice_history_id)
-  if(!practice_history_idExist) {
-    return socket.emit('error', MESSAGE.PRACTICE.NOT_EXIST)
+  const { userAnswer, order } = value
+  const correct_answer = socket.allQuestionreuploaded.find(
+    (q) => q.order === order
+  )?.correct_answer
+  let questionIndex = socket.allQuestionreuploaded.findIndex(
+    (q) => q.order === order
+  )
+  if (questionIndex !== -1) {
+    socket.allQuestionreuploaded[questionIndex].userAnswer = userAnswer
   }
-  const QuestionExist = await isQuestionExist(question_id)
-  if(!QuestionExist) {
-    return socket.emit('error', MESSAGE.DOCUMENT.NO_QUESTION)
-  }
-  const correct_answer = await practiceService.selectCorrectAnswer(question_id)
-  await practiceService.updatePracticeHistory(userAnswer ,question_id,practice_history_id)
-  
+
   if (userAnswer == correct_answer) {
     return callback('Correct')
-  }
-  else {
+  } else {
     return callback('Incorrect')
   }
 }
 
-
-const finished = async (socket,data,callback) => {
-  const {error,value} = practiceSchema.finishedValidate.validate(data)
+const finished = async (socket, data, callback) => {
+  const { error, value } = practiceSchema.finishedValidate.validate(data)
   if (error) {
     return socket.emit('error', error.details[0].message)
-  } 
-  const {practice_history_id,score}=value
-  const practice_history_idExist = await practiceService.ispractice_history_idExist(practice_history_id)
-  if(!practice_history_idExist) {
-    return socket.emit('error', MESSAGE.PRACTICE.NOT_EXIST)
   }
-  await practiceService.updateScore(score,practice_history_id)
-  const result = await practiceService.selectPracticeHistorybyID(practice_history_id)
+  const { user_id } = socket.user
+  socket.end_time = new Date().toISOString()
+  const { score } = value
+  const practice_history_id = await practiceService.insertPracticeHistory(
+    user_id,
+    score,
+    socket.allQuestionreuploaded,
+    socket.start_time,
+    socket.end_time
+  )
+  const result = await practiceService.selectPracticeHistorybyID(
+    practice_history_id
+  )
   return callback(result)
 }
 
-export default {  getNewQuestion, postUserAnswer,getStarted,finished }
+export default { getNewQuestion, postUserAnswer, getStarted, finished }
