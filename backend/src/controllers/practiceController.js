15ce34205c06a7ffd6103e96dc248feb96a3490b
NguyenDucTam <tam9898980@gmail.com>
2025-03-15 17:06:52 +0700

    feature: implement select documents by the same author

--- backend/src/controllers/practiceController.js
+++ backend/src/controllers/practiceController.js
@@ -45,13 +45,15 @@ const getDocumentforPratice = async (req, res) => {
 }
 const getPracticeHistory = async (req, res) => {
   const { user_id } = req.user
-  const { error, value } =
-    practiceSchema.getPracticeHistoryValidate.validate(req.query)
+  const { error, value } = practiceSchema.getPracticeHistoryValidate.validate(
+    req.query
+  )
   if (error) {
     return sendResponse(res, STATUS_CODE.BAD_REQUEST, error.details[0].message)
   }
   const { page, limit } = value
-  const {totalPracticeHistory, totalPages} = await practiceService.countPracticeHistory(user_id, limit)
+  const { totalPracticeHistory, totalPages } =
+    await practiceService.countPracticeHistory(user_id, limit)
   if (totalPracticeHistory == 0) {
     return sendResponse(
       res,
@@ -66,7 +68,11 @@ const getPracticeHistory = async (req, res) => {
       MESSAGE.PRACTICE.PAGE_EXCEEDS_TOTAL
     )
   }
-  const history = await practiceService.selectPracticeHistory(user_id,limit,page)
+  const history = await practiceService.selectPracticeHistory(
+    user_id,
+    limit,
+    page
+  )
   return sendResponse(
     res,
     STATUS_CODE.SUCCESS,
@@ -79,4 +85,50 @@ const getPracticeHistory = async (req, res) => {
     }
   )
 }
-export default { getDocumentforPratice,getPracticeHistory }
+
+const getDocumentbyUserID = async (req, res) => {
+  const { error, value } =
+    //validate mới
+    practiceSchema.getDocumentbyUserIDValidate.validate(req.query)
+  if (error) {
+    return sendResponse(res, STATUS_CODE.BAD_REQUEST, error.details[0].message)
+  }
+  const { page, limit, user_id } = value
+  const isUserExist = await practiceService.isUserIDExist(user_id)
+  if (!isUserExist) {
+    return sendResponse(res, STATUS_CODE.NOT_FOUND, MESSAGE.USER.NOT_FOUND)
+  }
+  const { totalPages, totalDocs } =
+    await practiceService.countDocumentsByUserId(user_id, limit)
+  if (totalDocs == 0) {
+    return sendResponse(
+      res,
+      STATUS_CODE.NOT_FOUND,
+      MESSAGE.PRACTICE.NOT_FOUND_DOCS_BY_CREATER
+    )
+  }
+  if (page > totalPages) {
+    return sendResponse(
+      res,
+      STATUS_CODE.BAD_REQUEST,
+      MESSAGE.PRACTICE.PAGE_EXCEEDS_TOTAL
+    )
+  }
+  const docs = await practiceService.selectDocumentbyUserId(
+    user_id,
+    limit,
+    page
+  )
+  return sendResponse(
+    res,
+    STATUS_CODE.SUCCESS,
+    MESSAGE.PRACTICE.GET_DOCS_SUCCESS,
+    {
+      docs: docs,
+      currentPage: page,
+      totalPages,
+      totalDocs,
+    }
+  )
+}
+export default { getDocumentforPratice, getPracticeHistory,getDocumentbyUserID }
